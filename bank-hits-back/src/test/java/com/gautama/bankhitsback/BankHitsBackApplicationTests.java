72436c41fccf1036789b4748fb2e231b37442df2
Egor Morozov <egornexus10@gmail.com>
2026-02-24 05:18:41 +0700

    init commit

---
+++ bank-hits-back/src/test/java/com/gautama/bankhitsback/BankHitsBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gautama.bankhitsback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankHitsBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
