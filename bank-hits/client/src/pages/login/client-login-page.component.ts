0176cfb1a8ee843efa188a07351028a10f4ea13a
NickolayRogov <kolya190509@mail.ru>
2026-03-01 19:12:37 +0700

    Поправиль роутинг по страницам. Поправил страницу входа

--- bank-hits/client/src/pages/login/client-login-page.component.ts
+++ bank-hits/client/src/pages/login/client-login-page.component.ts
@@ -1,16 +1,23 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { AuthRoleService } from '../../../../shared/auth';
 
 @Component({
   selector: 'app-client-login-page',
   standalone: true,
+  imports: [FormsModule],
   templateUrl: './client-login-page.component.html',
   styleUrl: './client-login-page.component.scss',
 })
 export class ClientLoginPageComponent {
   private readonly router = inject(Router);
+  private readonly authRoleService = inject(AuthRoleService);
 
-  register(): void {
-    void this.router.navigate(['/dashboard']);
+  protected email = signal('');
+
+  submit(): void {
+    this.authRoleService.setRole('client');
+    void this.router.navigate(['/panel']);
   }
 }
