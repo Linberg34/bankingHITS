e7c84ce4ecca3d364a2e7a7f5d519a8ab3b376b4
Sofya Kovalenko <sofyakovalenko2005610@gmail.com>
2026-03-22 02:03:24 +0700

    fix

---
+++ employee-bff/src/main/java/com/iisovaii/employee_bff/config/SwaggerConfig.java
@@ -0,0 +1,24 @@
+package com.iisovaii.employee_bff.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Employee BFF API",
+                version = "v1",
+                description = "API для приложения сотрудника банка"
+        )
+)
+@SecurityScheme(
+        name = "Bearer Authentication",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT",
+        description = "Введите JWT токен полученный от SSO сервиса"
+)
+public class SwaggerConfig {}
